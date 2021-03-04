7c59e68c44daf94c92d5d00d2b2045dc998b7790
“Andriy <andriy.danylko@insert.com.pl>
2021-03-04 11:18:59 +0100

    update

--- src/app/playground/use-custom-select/use-custom-select.component.ts
+++ src/app/playground/use-custom-select/use-custom-select.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-use-custom-select',
@@ -7,23 +6,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./use-custom-select.component.scss'],
 })
 export class UseCustomSelectComponent implements OnInit {
-  form: FormGroup;
+  constructor() {}
 
-  options = [
-    { title: 'Bananas', value: 'ban' },
-    { title: 'Apples', value: 'app' },
-  ];
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      option: ['', [Validators.required]],
-    });
-  }
-
-  checkValue() {
-    // this.form.controls.option.markAsPending();
-    console.log(this.form.controls.option);
-  }
+  ngOnInit() {}
 }
