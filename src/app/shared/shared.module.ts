4f778ead0f4af8270c7c64bc7479251134faba7c
“Andriy <andriy.danylko@gmail.com>
2021-03-04 14:50:34 +0100

    new ref

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ModalModule } from './modal/modal.module';
-import { ResponsivenessService } from '../shared/responsiveness.service';
 
 const COMPONENTS = [CommonModule, FormsModule, ReactiveFormsModule, ModalModule];
 
 @NgModule({
   imports: [COMPONENTS],
-  providers: [ResponsivenessService],
+  providers: [],
   declarations: [PageNotFoundComponent],
   exports: [COMPONENTS],
 })
