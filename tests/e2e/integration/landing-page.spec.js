cf08e937bddc8b145550de6fb65fd6a798e4c6a7
“Andriy <andriy.danylko@gmail.com>
2021-04-07 12:47:38 +0200

    add test cy

--- tests/e2e/integration/landing-page.spec.js
+++ tests/e2e/integration/landing-page.spec.js
@@ -3,6 +3,6 @@
 describe('Home Page', () => {
   it('should display the app name on the home page', () => {
     cy.visit('/');
-    cy.get('.my-test').should('contain.text', 'Test1');
+    cy.get('.my-test').should('contain.text', 'Test');
   });
 });
