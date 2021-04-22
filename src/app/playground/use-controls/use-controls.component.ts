97ca3a0f777d75f75f07b802d37022a985a4b090
“Andriy <andriy.danylko@gmail.com>
2021-04-22 15:39:26 +0200

    ref

--- src/app/playground/use-controls/use-controls.component.ts
+++ src/app/playground/use-controls/use-controls.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { wrapApiFormPost } from 'app/forms/form-helpers';
+
+import { UsePaginationUserApiService } from '../use-pagination/use-pagination-user-api-service';
+
 @Component({
   selector: 'app-use-controls',
   templateUrl: './use-controls.component.html',
@@ -14,15 +18,19 @@ export class UseControlsComponent implements OnInit {
     { name: 'radio 2', value: 'radio2' },
   ];
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private api: UsePaginationUserApiService) {}
 
   ngOnInit(): void {
     this.formGroup = this.fb.group({
-      radio: ['radio2', [Validators.required]],
+      radio: ['', [Validators.required]],
       time: ['', [Validators.required]],
       select: ['', [Validators.required]],
       checkbox: ['', [Validators.requiredTrue]],
       name: ['', [Validators.required]],
     });
+  }
+
+  submit() {
+    wrapApiFormPost(this.formGroup, () => this.api.postUser(this.formGroup.value));
   }
 }
