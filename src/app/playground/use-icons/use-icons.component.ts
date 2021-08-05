408d94992b5564f767fb3dbb16fbe99c48291727
“Andriy <andriy.danylko@gmail.com>
2021-08-05 17:19:20 +0200

    tic tac

--- src/app/playground/use-icons/use-icons.component.ts
+++ src/app/playground/use-icons/use-icons.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-use-icons',
   templateUrl: './use-icons.component.html',
   styleUrls: ['./use-icons.component.scss'],
 })
-export class UseIconsComponent {}
+export class UseIconsComponent implements OnInit {
+  currentPlayer = 'X' ? 'O' : 'X';
+  changePlayer = false;
+
+  arrays = [
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+  ];
+
+  ngOnInit() {}
+
+  changeValue(row, index, item) {
+    this.changePlayer = !this.changePlayer;
+
+    this.arrays[row][index] = !!item;
+  }
+}
