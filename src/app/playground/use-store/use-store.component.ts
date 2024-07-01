cdf0c795b2cbed67a3cf37969324d7f98fdb3d74
“Andriy <andriy.danylko@gmail.com>
2024-07-01 14:48:37 +0200

    ver 17+

---
+++ src/app/playground/use-store/use-store.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-use-store',
+  templateUrl: './use-store.component.html',
+  styleUrls: ['./use-store.component.scss'],
+})
+export class UseStoreComponent {
+  constructor() {}
+
+  public increase() {}
+
+  public decrease() {}
+
+  public clear() {}
+}
